d02d5083b7f024db5272aef14c4717854679a5da
Maheshdeekonda428 <maheshdeekonda45@gmail.com>
2024-04-18 12:48:00 +0530

    Project-2 added

---
+++ Project-2/src/data.js
@@ -0,0 +1,157 @@
+import {
+  CSS3,
+  Express,
+  HTML5,
+  Javascript,
+  Mongodb,
+  Nextjs,
+  Nextjs2,
+  Nodejs,
+ 
+  Reactjs,
+  admin,
+  food,
+  food1,
+  port1,
+  port2,
+  port3,
+  social,
+  Python,
+} from "./assets";
+
+export const projectStats = [
+  {
+    name: "Total Projects",
+    value: "10+",
+  },
+  {
+    name: "Website Design",
+    value: "15+",
+  },
+
+];
+
+export const toolsofme = [
+  {
+    name: "MongoDB",
+    icon: Mongodb,
+  },
+  {
+    name: "Express",
+    icon: Express,
+  },
+  {
+    name: "React",
+    icon: Reactjs,
+  },
+  {
+    name: "NodeJs",
+    icon: Nodejs,
+  },
+  {
+    name: "NextJs",
+    icon: Nextjs2,
+  },
+];
+
+export const skills = [
+  {
+    name: "ReactJs",
+    value: 80,
+    icon: Reactjs,
+  },
+  {
+    name: "NodeJs",
+    value: 75,
+    icon: Nodejs,
+  },
+  {
+    name: "NextJs",
+    value: 80,
+    icon: Nextjs,
+  },
+  {
+    name: "MongoDB",
+    value: 80,
+    icon: Mongodb,
+  },
+  {
+    name: "CSS3",
+    value: 90,
+    icon: CSS3,
+  },
+  {
+    name: "HTML5",
+    value: 90,
+    icon: HTML5,
+  },
+  {
+    name: "JavaScript",
+    value: 85,
+    icon: Javascript,
+  },
+  {
+    name: "python",
+    value: 75,
+    icon: Python,
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Admin Dashboard",
+    cat: "Web Design",
+    url: "",
+    img: admin,
+  },
+  {
+    id: 2,
+    title: "Portfolio Website",
+    cat: "Web Design",
+    url: "",
+    img: port1,
+  },
+  {
+    id: 3,
+    title: "Portfolio Website",
+    cat: "Web Design",
+    url: "",
+    img: port2,
+  },
+  {
+    id: 4,
+    title: "MTS Website",
+    cat: "Web Design",
+    url: "",
+    img: port3,
+  },
+  {
+    id: 5,
+    title: "Eat Well Website",
+    cat: "Web Design",
+    url: "",
+    img: food,
+  },
+  // {
+  //   id: 6,
+  //   title: "Social Media",
+  //   cat: "Web App",
+  //   url: "",
+  //   img: social,
+  // },
+
+];
+
+export const softskills = [
+
+  "Communication",
+  "Empathy",
+ 
+  "Helpfulness",
+  "Creativity",
+  "Self-learning",
+  "Accountability",
+  "Problem solving",
+  "Open-mindedness",
+];
